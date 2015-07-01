9924547d923bb8e5c807aa7443796d490360ff01
Jeremy <jchatkinson@gmail.com>
2015-06-30 22:34:27 -0700

    Snapshot before playing with plotCtrl and databinding issue

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -20,26 +20,68 @@
 		.module('application')
   		.service('mydata', MyData);
 	
+	
+	//define a function that creates a sine time series
+	function createHarmonicTimeSeries(functype,T,A,hShift,vShift,dt,time) {
+		var npts = Math.ceil(time / dt);
+		var myseries = [];
+		switch(functype) {
+			case "sine":
+				var eqn = function(A,T,hShift,t){
+					return A*Math.sin(2*Math.PI/T*(t+hShift))+vShift;
+				} 
+				break;
+			case "saw":
+				var eqn = function(A,T,hShift,t){
+					return -2*A/Math.PI*Math.atan( 1/Math.tan( ( t+hShift )*Math.PI/T ) ) + vShift;
+				} 
+				break;
+			case "square":
+				var eqn = function(A,T,hShift,t){
+					return A*( 2*Math.floor((t+hShift)/T) - Math.floor(2*(t+hShift)/T) + 1 )+vShift;
+				} 
+				break;
+			} 
+		var yt=0;
+		var i;
+		for (i=0; i<npts-1; i++) {
+			var t=i*dt
+			yt = eqn(A,T,hShift,t);
+			myseries.push({
+				x:t,
+				y:yt
+			}); 			
+		}
+		//add 10 seconds of additional points of zeros
+		var extraPts = Math.ceil(10/dt); 
+		for (i=npts; i<npts+extraPts; i++) {
+			t=i*dt
+			myseries.push({
+				x:t,
+				y:0
+			}); 			
+		}		
+		return myseries;
+	}
+	
+	
 	//define a controller for the Excitation template that uses the mydata service
 	function ExcitationCtrl(mydata) {
-    	var excite = this;
-    	excite.mydata = mydata;
-    	excite.frcfuns = [{name:'harmonic',fullname:'Harmonic Function'},{name:'impulse',fullname:'Impulse Load'},{name:'file',fullname:'Time History from File'}];
-		excite.selectedfrcfun = excite.frcfuns[0].name; 	
-	 	excite.harmfuns = [{name:'sine', fullname:'Sine Wave'},{name:'saw', fullname:'Sawtooth Wave'},{name:'square', fullname:'Square Wave'}];
-	    excite.selectedharmfun = excite.harmfuns[0].name; // default to sine function
-	    excite.T = 0.5;
-	    excite.A = 9.8; 
-	    excite.Ph = 0;
-	    excite.dt = 0.01;
-	    excite.totalTime = 5.0;
-	    excite.SF = 1.0;
-	    //function to push the excitefunc options to mydata
-	    excite.submit = function () {
-	    		//clear the existing mydata.myexcitefunc
-	    	
-	    		//push the required info to mydata.myexcitefunc
-	    };
+    	var vm = this;
+    	vm.mydata = mydata;
+    	vm.frcfuns = [{name:'harmonic',fullname:'Harmonic Function'},{name:'impulse',fullname:'Impulse Load'},{name:'file',fullname:'Time History from File'}];
+		vm.selectedfrcfun = vm.frcfuns[0].name; 	
+	 	vm.harmfuns = [{name:'sine', fullname:'Sine Wave'},{name:'saw', fullname:'Sawtooth Wave'},{name:'square', fullname:'Square Wave'}];
+	    vm.mydata.myexcitefunc.selectedharmfun = vm.harmfuns[0].name; // default to sine function
+	    vm.mydata.myexcitefunc.T = 5;
+	    vm.mydata.myexcitefunc.A = 1.0; 
+	    vm.mydata.myexcitefunc.Ph = 0;
+	    vm.mydata.myexcitefunc.dt = 0.1;
+	    vm.mydata.myexcitefunc.totalTime = 10.0;
+	    vm.mydata.myexcitefunc.SF = 1.0;
+	    vm.mydata.myexcitefunc.vShift = 0;
+	    vm.mydata.myexcitefunc.hShift = 0;
+	    vm.mydata.myexcitefunc.series = createHarmonicTimeSeries( vm.mydata.myexcitefunc.selectedharmfun, vm.mydata.myexcitefunc.T, vm.mydata.myexcitefunc.A, vm.mydata.myexcitefunc.hShift, vm.mydata.myexcitefunc.vShift, vm.mydata.myexcitefunc.dt, vm.mydata.myexcitefunc.totalTime);
   	};
 	angular
 		.module('application')
@@ -57,12 +99,13 @@
 		.controller('SystemPropCtrl', SystemPropCtrl);  
   	
   	//define a controller for the excitation plot
-  	function LineCtrl($scope) {
-		    //$scope.data = [{x:0,val_0:0},{x:1,val_0:0.993},{x:2,val_0:1.947},{x:3,val_0:2.823},{x:4,val_0:3.587},{x:5,  val_0:4.207},{x:6,  val_0:4.66},{x:7,  val_0:4.927},{x:8,  val_0:4.998},{x:9,val_0:4.869},{x:10,val_0:4.546},{x:11,val_0:4.042},{x:12,val_0:3.377},{x:13,val_0:2.578},{x:14,val_0:1.675},{x:15,val_0:0.706}];	
-		    $scope.data = [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:3,y:0},{x:4,y:0},{x:5,y:0}];
-		    $scope.options = {
-		      series: [{y: "y",label: 'My forcing function',color: "#1f77b4",type: "area"}]
-		    };
+ 	function LineCtrl(mydata) {
+ 		var vm = this;
+ 		vm.mydata = mydata;
+		//vm.plotdata = [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:3,y:0},{x:4,y:0},{x:5,y:0}];
+	    vm.plotoptions = {
+			series: [{y: "y",label: 'My forcing function',color: "#1f77b4",type: "area"}]
+	    };
 	};
 	angular
 		.module('application')		
