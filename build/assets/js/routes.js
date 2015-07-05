a7d961375f2551ba1aa20ff6ce574abc32b7699b
Jeremy <jchatkinson@gmail.com>
2015-07-05 12:30:04 -0700

    Added entire repo to tracked files

---
+++ build/assets/js/routes.js
@@ -0,0 +1,1 @@
+var foundationRoutes = [{"name":"system","url":"/system/","path":"templates/system.html"},{"name":"response","url":"/response/","path":"templates/response.html"},{"name":"excitation","url":"/excitation/","path":"templates/excitation.html"},{"name":"about","url":"/about/","path":"templates/about.html"},{"name":"home","url":"/","path":"templates/home.html"}]; 
