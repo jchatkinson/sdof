5f5ae787e10028064ddfa8033f34bdb3c6a4acb4
Jeremy <jchatkinson@gmail.com>
2015-07-05 23:48:16 -0700

    Update 2015-07-06T06:48:14.284Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,27 +14,20 @@
   		var mydata = this;
   		mydata.sT = 0.5;
   		mydata.sDamp = 5;
-  		mydata.eFun = {type:'harmonic',subtype:'sine'}; // default to sine function
+  		mydata.eFun = [];
 	    mydata.eT = 1.0;
 	    mydata.eA = 1.0; 
 	    mydata.edt = 0.01;
-	    mydata.etotalTime = 5.0;
+	    mydata.eTotalTime = 10.0;
+	    mydata.eExciteTime = 5.0;
 	    mydata.eSF = 1.0;
 	    mydata.eVShift = 0;
 	    mydata.eHShift = 0;
-	    mydata.eSeries = createHarmonicTimeSeries( mydata.eFun.subtype, mydata.eT, mydata.eA, mydata.eHShift, mydata.eVShift, mydata.edt, mydata.etotalTime);
-  		mydata.response = analyzeSystem(mydata.sT,mydata.sDamp,mydata.eSeries,mydata.edt,mydata.etotalTime);
+	    mydata.eSeries = [{x:0,y:0},{x:1,y:0}];
+  		mydata.response = analyzeSystem(mydata.sT,mydata.sDamp,mydata.eSeries,mydata.edt,mydata.eTotalTime);
 	  	//mydata.onChange = function () {
-	  	//	mydata.eSeries = createHarmonicTimeSeries( mydata.eFun.subtype, mydata.eT, mydata.eA, mydata.eHShift, mydata.eVShift, mydata.edt, mydata.etotalTime);
-	  	//}
-		//mydata.fileupload = function(){
-		//	var userfile = document.getElementById("userfile").files[0];
-	    //    var reader = new FileReader();
-	    //  	reader.onloadend = function(){
-	    //    	mydata.eSeries = reader.result;
-	    //  	}
-	    //  	reader.readAsText(userfile);
-	    //}	  		  	
+	  	//	mydata.eSeries = createHarmonicTimeSeries( mydata.eFun.name, mydata.eT, mydata.eA, mydata.eHShift, mydata.eVShift, mydata.edt, mydata.etotalTime);
+	  	//}	  	
 	  	//mydata.calcResponse = function() {
 	  	//	mydata.response = analyzeSystem(mydata.sT,mydata.sDamp,mydata.eSeries,mydata.edt,mydata.etotalTime);
 	  	//}
@@ -44,46 +37,68 @@
   		.service('mydata', MyData);
 	
 	//define a function that creates a sine time series
-	function createHarmonicTimeSeries(functype,T,A,hShift,vShift,dt,time) {
-		var npts = Math.ceil(time / dt);
-		var myseries = [];
+	function createTimeSeries(mydata) {
+		var npts = Math.ceil(mydata.eExciteTime / mydata.edt);
+		var functype = mydata.eFun.name;		
+		mydata.eSeries = [];
 		switch(functype) {
 			case 'sine':
-				var eqn = function(A,T,hShift,t){
-					return A*Math.sin(2*Math.PI/T*(t+hShift))+vShift;
+				var eqn = function(mydata,t){
+					return mydata.eA*Math.sin(2*Math.PI/mydata.eT*(t+mydata.eHShift))+mydata.eVShift;
 				} 
 				break;
 			case 'saw':
-				var eqn = function(A,T,hShift,t){
-					return -2*A/Math.PI*Math.atan( 1/Math.tan( ( t+hShift )*Math.PI/T ) ) + vShift;
+				var eqn = function(mydata,t){
+					return -2*mydata.eA/Math.PI*Math.atan( 1/Math.tan( ( t+mydata.eHShift )*Math.PI/mydata.eT ) ) + mydata.eVShift;
 				} 
 				break;
 			case 'square':
-				var eqn = function(A,T,hShift,t){
-					return A*( 2*Math.floor((t+hShift)/T) - Math.floor(2*(t+hShift)/T) + 1 )+vShift;
+				var eqn = function(mydata,t){
+					return mydata.eA*( 2*Math.floor((t+mydata.eHShift)/mydata.eT) - Math.floor(2*(t+mydata.eHShift)/mydata.eT) + 1 )+mydata.eVShift;
 				} 
 				break;
-			} 
-		var yt=0;
-		var i;
+			case 'rampup':
+				console.log('rampup not implemented yet');
+				var eqn = function () { return 0 }
+				break;
+			case 'rampdn':
+				console.log('rampdn not implemented yet');
+				var eqn = function () { return 0 }
+				break;
+			case 'acc':
+				console.log('acc not implemented yet');
+				var eqn = function () { return 0 }
+				break;
+			case 'timeacc':
+				console.log('timeacc not implemented yet');
+				var eqn = function () { return 0 }
+				break;
+			case 'peernga':
+				console.log('peernga not implemented yet');
+				var eqn = function () { return 0 }
+				break;				
+			}; 
+		var yt=0.0;
+		var i,t;
 		for (i=0; i<npts-1; i++) {
-			var t=i*dt
-			yt = eqn(A,T,hShift,t);
-			myseries.push({
+			t=i*mydata.edt;
+			yt = eqn(mydata,t);
+			mydata.eSeries.push({
 				x:t,
 				y:yt
 			}); 			
 		}
-		//add 1 seconds of additional points of zeros
-		var extraPts = Math.ceil(1/dt); 
-		for (i=npts; i<npts+extraPts; i++) {
-			t=i*dt
-			myseries.push({
-				x:t,
-				y:0
-			}); 			
-		}		
-		return myseries;
+		//add additional points of zeros if eTotalTime > eExciteTime
+		if (mydata.eTotalTime > mydata.eExciteTime) {
+			for (i=npts; i<npts+Math.ceil((mydata.eTotalTime - mydata.eExciteTime)/mydata.edt); i++) {
+				t=i*mydata.edt;
+				mydata.eSeries.push({
+					x:t,
+					y:0.0
+				}); 			
+			}	
+		};
+		return mydata;
 	}
 	
 	//define a function to analyze the system
@@ -137,43 +152,48 @@
 		};
 	}
 	
+	//define a function to parse user file
+	function fileupload(){
+		var userfile = document.getElementById("userfile").files[0];
+	    var reader = new FileReader();
+	    reader.onloadend = function(){
+	    	return reader.result
+	       		
+      	};
+      	reader.readAsText(userfile);
+    }		
+	
 	//define a controller for the Excitation template that uses the mydata service
 	function ExcitationCtrl(mydata) {
     	var vm = this;
     	vm.mydata = mydata;
-    	vm.funtypes = [{name:'harmonic',fullname:'Harmonic Function'},{name:'impulse',fullname:'Impulse Load'},{name:'file',fullname:'Time History File'}];
-    	vm.funsubtypes = function () {
-    		if ( vm.isTypeSelected(0) ) { 
-    			return [{name:'sine', fullname:'Sine Wave'},{name:'saw', fullname:'Sawtooth Wave'},{name:'square', fullname:'Square Wave'}];
-			};
-    		if ( vm.isTypeSelected(1) ) {  
-    			return [{name:'rampup', fullname:'Ramp Up'},{name:'rampdn', fullname:'Ramp Down'}];
-    		};
-    		if ( vm.isTypeSelected(2) ) {  
-    			return [{name:'peer',fullname:'PEER NGA File',note:'Note: The file must formatted as an unaltered <a href="http://ngawest2.berkeley.edu/" target="_blank">PEER NGA Record</a>'},
- 						{name:'acc',fullname:'Acceleration',note:'Note: The file must formatted such that each row contains <code>[acceleration]</code> at a specified time interval'},
- 						{name:'timeacc',fullname:'Time and Acceleration',note:'Note: The file must formatted such that each row contains <code>[time,acceleration]</code>'}];
-    		};    		
-    	};
-	 	vm.isSubtypeSelected = function (checkind) {
-	 		vm.funsubtypes();
-	 		return vm.mydata.eFun.subtype === vm.funsubtypes[checkind].name;
+    	vm.funtypes = [	{name:'sine', fullname:'Sine Wave',type:'Harmonic',note:''},
+    					{name:'saw', fullname:'Sawtooth Wave',type:'Harmonic',note:''},
+    					{name:'square', fullname:'Square Wave',type:'Harmonic',note:''},
+    					{name:'rampup', fullname:'Ramp Up',type:'Impulse',note:''},
+    					{name:'rampdn', fullname:'Ramp Down',type:'Impulse',note:''},
+ 						{name:'acc',fullname:'Acceleration',type:'From File',note:'Note: The file must formatted such that each row contains <code>[acceleration]</code> at a specified time interval'},
+ 						{name:'timeacc',fullname:'Time and Acceleration',type:'From File',note:'Note: The file must formatted such that each row contains <code>[time,acceleration]</code>'},
+ 						{name:'peernga',fullname:'PEER NGA File',type:'From File',note:'Note: The file must formatted as an unaltered <a href="http://ngawest2.berkeley.edu/" target="_blank">PEER NGA Record</a>'}
+ 						];
+	 	vm.mydata.eFun = vm.funtypes[0]; //initialize select to the sine function
+	 	vm.datacheckbox = false;
+	 	createTimeSeries(mydata);
+	 	vm.updateTS = function () {
+	 		createTimeSeries(mydata);
+ 		};
+	 	vm.isFunSelected = function(ind){
+	 		if (typeof ind === 'number') {
+	 			return vm.mydata.eFun === vm.funtypes[ind];
+	 		} else {
+	 			var i,isit; 
+	 			isit = false; 
+	 			for (i=0; i<ind.length; i++) {
+	 				isit = (isit || vm.mydata.eFun===vm.funtypes[ind[i]]);
+	 			};
+	 			return isit;
+	 		};
 	 	};
-	 	vm.isTypeSelected = function(checkind){
-	 		return vm.mydata.eFun.type === vm.funtypes[checkind].name;
-	 	};
-	 	vm.fileupload = function(){
-			var userfile = document.getElementById("userfile").files[0];
-	        var reader = new FileReader();
-	      	reader.onloadend = function(){
-	       		vm.mydata.eSeries = reader.result;
-	       		
-	      	};
-	      	reader.readAsText(userfile);
-	    };	  		     	
-    	//mydata.onChange = function () {
-	  	//	mydata.eSeries = createHarmonicTimeSeries( mydata.eFun.subtype, mydata.eT, mydata.eA, mydata.eHShift, mydata.eVShift, mydata.edt, mydata.etotalTime);
-	  	//}
 	};
 	angular
 		.module('application')
@@ -195,7 +215,7 @@
 		var vm = this;
 		vm.mydata = mydata;
 		vm.calcResponse = function() {
-	  		vm.mydata.response = analyzeSystem(vm.mydata.sT,vm.mydata.sDamp,vm.mydata.eSeries,vm.mydata.edt,vm.mydata.etotalTime);
+	  		vm.mydata.response = analyzeSystem(vm.mydata.sT,vm.mydata.sDamp,vm.mydata.eSeries,vm.mydata.edt,vm.mydata.eTotalTime);
 	  	};
 		
 	};
@@ -240,10 +260,10 @@
 
   	function config($urlProvider, $locationProvider) {
     	$urlProvider.otherwise('/');
-    	//$locationProvider.html5Mode({
-      	//	enabled:false,
-      	//	requireBase: false
-    	//});
+    	$locationProvider.html5Mode({
+      		enabled:false,
+      		requireBase: false
+    	});
     	$locationProvider.hashPrefix('!');
   	};
 
